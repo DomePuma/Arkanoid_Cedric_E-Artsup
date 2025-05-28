4d7cd36be576b52afe3ff169a3a8144707a35d2b
LaPetiteDev <lahessaimere@gmail.com>
2025-05-28 21:16:24 +0200

    Change scene options et credits

--- Assets/05-Scripts/Menu/First Menu/MenuController.cs
+++ Assets/05-Scripts/Menu/First Menu/MenuController.cs
@@ -6,28 +6,10 @@ namespace BrickBreaker.Menu.ChangeScene
     public class MainMenuController : MonoBehaviour
     {
         [SerializeField] private string _gameSceneName;
-        [SerializeField] private string _optionsSceneName;
-        [SerializeField] private string _creditsSceneName;
 
         public void PlayGame()
         {
             SceneManager.LoadScene(_gameSceneName);
-        }
-
-        public void OpenOptions()
-        {
-            if (!SceneManager.GetSceneByName(_optionsSceneName).isLoaded)
-            {
-                SceneManager.LoadScene(_optionsSceneName, LoadSceneMode.Additive);
-            }
-        }
-
-        public void OpenCredits()
-        {
-            if (!SceneManager.GetSceneByName(_creditsSceneName).isLoaded)
-            {
-                SceneManager.LoadScene(_creditsSceneName, LoadSceneMode.Additive);
-            }
         }
 
         public void QuitGame()
