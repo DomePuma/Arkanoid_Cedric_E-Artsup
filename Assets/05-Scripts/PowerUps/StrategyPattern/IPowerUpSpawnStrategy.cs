6c64ee3752d5a272b38941463a3fa23143ccb199
FT-Fumetsu <esmieudamien18@gmail.com>
2025-05-31 17:57:16 +0200

    Remove Useless Comment

--- Assets/05-Scripts/PowerUps/StrategyPattern/IPowerUpSpawnStrategy.cs
+++ Assets/05-Scripts/PowerUps/StrategyPattern/IPowerUpSpawnStrategy.cs
@@ -3,6 +3,6 @@ namespace BrickBreaker.Spawning.PowerUp.Strategy
     public interface IPowerUpSpawnStrategy
     {
         bool ShouldSpawnPowerUp();
-        int ChoosePowerUpIndex(int count); // Peut être optionnel selon les besoins
+        int ChoosePowerUpIndex(int count);
     }
 }
